8cc1548d30e167c4bbd0b42c569d41a803c72c66
angelodias <angelossdias@gmail.com>
2020-03-31 17:59:55 -0300

    First commit: first lesson finished

---
+++ app/src/main/java/br/com/training/android/zooapp/Animal.kt
@@ -0,0 +1,7 @@
+package br.com.training.android.zooapp
+
+class Animal(name: String, description: String, image: Int) {
+    var name: String? = name
+    var description: String? = description
+    var image: Int? = image
+}
